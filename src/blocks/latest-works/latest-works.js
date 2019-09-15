17e39371a3f841eab32e2ea24eb6aa7d718373cd
encrypticus <encrypticus@gmail.com>
2019-09-15 18:45:10 +0600

    Добавил один новый блок и два новых компонента
    Добавил блок latest-works и компоненты slide и arrow-button.

---
+++ src/blocks/latest-works/latest-works.js
@@ -0,0 +1,43 @@
+const slider = document.querySelector('.latest-works__slider');
+const sliderInner = slider.querySelector('.latest-works__slider-inner');
+const buttonLeft = document.querySelector('.arrow-button');
+const buttonRight = document.querySelector('.arrow-button_direction_right');
+const { forEach } = Array.prototype;
+const touchEventDispathcer = new Hammer(slider);
+let percentForMarginLeft = 0;
+
+const setMarginLeft = function setMarginLeft() {
+  sliderInner.style.marginLeft = `${percentForMarginLeft}%`;
+};
+
+const checkLeftBorder = function checkLeftBorder() {
+  if (percentForMarginLeft < -400) {
+    percentForMarginLeft = -400;
+    console.log(percentForMarginLeft);
+    return;
+  }
+};
+
+const checkRightBorder = function checkRightBrder() {
+  if (percentForMarginLeft > 0) {
+    percentForMarginLeft = 0;
+    return;
+  }
+};
+
+const moveLeft = function moveLeft() {
+  percentForMarginLeft -= 100;
+  checkLeftBorder();
+  setMarginLeft();
+};
+
+const moveRight = function moveRight() {
+  percentForMarginLeft += 100;
+  checkRightBorder();
+  setMarginLeft();
+};
+
+buttonRight.addEventListener('click', moveLeft);
+buttonLeft.addEventListener('click', moveRight);
+touchEventDispathcer.on('swipeleft', moveLeft);
+touchEventDispathcer.on('swiperight', moveRight);
--- src/js/index.js
+++ src/js/index.js
@@ -2,3 +2,4 @@ import '../sass/styles.scss';
 import '../components/main-nav/main-nav';
 import '../components/burger/burger';
 import '../blocks/banner/banner';
+import '../blocks/latest-works/latest-works';
